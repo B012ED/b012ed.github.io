e279a0f2fa1cf8e3b1597646278e3f2b4cbc4586
yusa <48654948+B012ED@users.noreply.github.com>
2021-06-07 10:01:20 +0800

    Update music.js

--- music.js
+++ music.js
@@ -1,5 +1,4 @@
 function play(){ 
    var audio = document.getElementById("bored"); audio.play("http://b012ed.github.io/b012ed.mp3");}
 
-oncontextmenu="return false" onkeydown="return false" onmousedown="return false"
 
