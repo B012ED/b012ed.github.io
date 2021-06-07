0dc2a75365061f529bea168c801a5d73b2a012d8
yusa <48654948+B012ED@users.noreply.github.com>
2021-06-07 09:56:22 +0800

    Update music.js

--- music.js
+++ music.js
@@ -1,35 +1,5 @@
-
+function play(){ 
+   var audio = document.getElementById("bored"); audio.play("http://b012ed.github.io/b012ed.mp3");}
 
 oncontextmenu="return false" onkeydown="return false" onmousedown="return false"
-
-function play(){ 
-   var audio = document.getElementById("mp3"); audio.play("012");}
-
-
-
-var music = Function.prototype.bind = Function.prototype.bind || function(fixThis) {
-        var func = this
-        return function() {
-            return func.apply(fixThis, arguments)
-            
-{
-  music = app.CreateMusic()
-  music.CreatePlayer( "http://b012ed.github.io/b012ed.mp3", "AutoPlay")
-}
-
-{
-  app.ShowDebug()
-  music = app.CreateMusic()
-  synth = music.CreateSynth()
-
-  // Call a function every 2 seconds
-  music.Loop(OnLoop, "+2").Start()
-  music.StartTransport()
-}
-
-function OnLoop(time)
-{
-  console.log(time)
-  synth.PlayStopTone("C4", "8n")
-}
 
