aec538f44d615973fd5b7005e1225a8f1df9442a
yusa <48654948+B012ED@users.noreply.github.com>
2021-06-07 10:18:41 +0800

    Update music.js

--- music.js
+++ music.js
@@ -1,4 +1,4 @@
-function play(){ 
-   var audio = document.getElementById("bored"); audio.play("http://b012ed.github.io/b012ed.mp3");}
+$(document).ready(function play(){
+var audio = document.getElementById("bored"); audio.play("http://b012ed.github.io/b012ed.mp3");}
 
 
